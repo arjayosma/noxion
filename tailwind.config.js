504a27d595318517c07cb8275bdda1cf333b2309
Arjay Osma <arjay.osma@gmail.com>
2021-08-27 15:53:00 +0800

    Update tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
     './layouts/**/*.{js,ts,jsx,tsx}',
     './pages/**/*.{js,ts,jsx,tsx}',
-    './utils/**/*.{js,ts,jsx,tsx}',
+    './util/**/*.{js,ts,jsx,tsx}',
   ],
   darkMode: false,
   theme: {
